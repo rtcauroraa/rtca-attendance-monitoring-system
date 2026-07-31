1d4d0a91542215d28df3ca64cf9292aec01e3c2f
rtcauroraa <rtcauroraa@gmail.com>
2026-07-31 17:42:39 +0800

    change

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,7 +3,7 @@ import AppLogoIcon from '@/components/app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <div className="flex size-14 items-center justify-center text-sidebar-primary-foreground">
+            <div className="flex size-12 items-center justify-center text-sidebar-primary-foreground">
                 <AppLogoIcon className="size-10 object-contain text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
